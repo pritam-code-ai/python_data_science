47b08e8360b853224b22314bfbbafcd6a6e55a5b
Pritam <42469889+pritam-code-ai@users.noreply.github.com>
2018-08-20 11:09:26 +0530

    Update save_graph_and_convert_file_to_csv.py

--- happy code/save_graph_and_convert_file_to_csv.py
+++ happy code/save_graph_and_convert_file_to_csv.py
@@ -1,1 +1,26 @@
 
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x_axis_1 = [-1, 1, 1.5, 9, 0, .9, .6, -9]
+
+np.savetxt("x_axis_1.csv", x_axis_1, delimiter=",", fmt='%s', header="x_axis_1")
+
+plt.plot(x_axis_1)
+
+plt.xlabel("x label of value list on x-axis")
+plt.ylabel("y label of value list on y-axis")
+
+# save graph in image file
+#if graph saved after show(), image file will not show the graph
+
+plt.savefig('a_simple_graph.png')
+
+plt.show()
+
+
+
+
+
